eb9255ff9f3e9376efd060ed4562913f43795366
sumonsheikh <sm.sumon117@gmail.com>
2021-03-12 23:22:51 +0600

    create firebase client

---
+++ firebaseClient.js
@@ -0,0 +1,10 @@
+const firebase = require("firebase");
+
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAegYMzt_CXh4-C-AWIrM0banCeGTxJ3f8",
+    authDomain: "nextjs-with-firebase-e8de9.firebaseapp.com",
+    projectId: "nextjs-with-firebase-e8de9",
+    storageBucket: "nextjs-with-firebase-e8de9.appspot.com",
+    messagingSenderId: "679043965958",
+    appId: "1:679043965958:web:e89f9f44b916fc67808f87",
+}
