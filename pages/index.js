31dc8cf27ef4e0f7eee16e95d4ffd07f3c02ca9c
sumonsheikh <sm.sumon117@gmail.com>
2021-03-16 23:24:32 +0600

    desing home page with chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,36 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Link from 'next/link';
+import {useAuth} from '../auth';
+import Container from '../components/Container';
+import {Flex, Box, Button, Text, Heading, Stack} from "@chakra-ui/react";
 
 export default function Home() {
+  const {user} = useAuth();
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Container>
+      <Flex>
+        <Box w={500} p={4} my={12} mx="auto">
+          <Heading as="h2" textAlign="center">
+              welcome to home page
+          </Heading>
+          <Text mt={8} textAlign="center">{`user id: ${user ? user.uid : "No user signed in"}`}</Text>
+          <Stack mt={8} alignItems="center" justifyContent="center" isInline width="100%">
+            <Button variant="solid" variantColor="blude" isDisabled={!user} width="100%">
+              <Link href="/authenticated">
+                <a>
+                        go to authenticated page
+                </a>
+              </Link>
+            </Button>
+            <Button width="100%" variant="solid" variantColor="green" isDisabled={user}>
+              <Link href="/login">
+                <a>Login</a>
+              </Link>
+            </Button>
+          </Stack>
+        </Box>
+      </Flex>
+    </Container>
+    
   )
 }
