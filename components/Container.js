b1ff962eea857ff4d5e706e7ac5723a1abf8895f
sumonsheikh <sm.sumon117@gmail.com>
2021-03-16 22:55:36 +0600

    create container component

---
+++ components/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Flex} from "@chakra-ui/react";
+export default function Container ({children}){
+    return(
+        <>
+            <Flex as ="main" justifyContent="center"
+            flexDirection="column" px = {8}>
+                {children}
+            </Flex>
+        </>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import {AuthProvider} from "../auth";
+import { ThemeProvider, CSSReset } from "@chakra-ui/react"; 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ThemeProvider>
+      <CSSReset/>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
